ff792aee0379d63b35bb663a1c878d67572b09dc
Hunter <jikemsa@gmail.com>
2025-10-22 15:37:40 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println("HW4 PT2 DECORATOR");
+    /*Problem notes:
+    (base object)food items: base price
+    (wrapper)toppings: modify price and name
+    (wrapper)loyalty status discount:modify final price
+    Customer order: list of food items
+    //Remember: "Veneration of Saints" and "Research Subsidies"
+    both correctly increasing research and unity produced
+    and energy upkeep for Knights of the Toxic God in Sellaris
+    */
+
+}
